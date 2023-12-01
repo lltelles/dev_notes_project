94be8a0f2d50c209de20c3c5c30dc6afb442f103
Lucas Telles <lltellesss@gmail.com>
2023-12-01 16:06:08 -0300

    added ordered fixed notes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const addNoteBtn = document.querySelector(".add-note");
 
 // funcoes
 function showNotes() {
+    cleanNotes()
+
     getNotes().forEach((note) => {
         const noteElement = createNote(note.id, note.content, note.fixed)
 
@@ -12,6 +14,10 @@ function showNotes() {
     })
 }
 
+
+function cleanNotes() {
+    notesContainer.replaceChildren([])
+}
 
 function addNote() {
 const notes = getNotes()
@@ -53,14 +59,42 @@ function createNote(id, content, fixed){
     // add textarea to the div
     element.appendChild(textarea)
 
+    const pinIcon = document.createElement('i')
+    pinIcon.classList.add(...['bi', 'bi-pin'])
+
+    element.appendChild(pinIcon)
+
+    if(fixed){
+        element.classList.add('fixed')
+    }
+
+    // eventos do elements
+    element.querySelector('.bi-pin').addEventListener('click', () =>{
+        toggleFixeNote(id)
+    })
+
     return element
+}
+
+function toggleFixeNote(id) {
+    const notes = getNotes()
+
+    const targetNotes = notes.filter((note) => note.id === id)[0]
+
+    targetNotes.fixed = !targetNotes.fixed
+
+    saveNotes(notes)
+
+    showNotes()
 }
 
 // Local Storage
 function getNotes(){
     const notes = JSON.parse(localStorage.getItem('notes') || "[]")
 
-    return notes
+    const orderedNotes = notes.sort((a, b) => a.fixed > b.fixed ? -1 : 1)
+
+    return orderedNotes
 }
 
 function saveNotes(notes) {
